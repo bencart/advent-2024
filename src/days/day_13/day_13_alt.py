bee2e96293a169adda4a85c158d76e88d2f44b0b
Ben Carter <ben@tearsofaunicorn.com>
2024-12-13 21:27:12 +0800

    Day 13

--- src/days/day_13/day_13_alt.py
+++ src/days/day_13/day_13_alt.py
@@ -1,32 +1,5 @@
-import re
-
 from common.input import get_data_file
-from days.day_13.day_13 import Machine, parse_machines
-
-TOKENS_A = 3
-TOKENS_B = 1
-
-EXAMPLE = """
-Button A: X+94, Y+34
-Button B: X+22, Y+67
-Prize: X=8400, Y=5400
-
-Button A: X+26, Y+66
-Button B: X+67, Y+21
-Prize: X=12748, Y=12176
-
-Button A: X+17, Y+86
-Button B: X+84, Y+37
-Prize: X=7870, Y=6450
-
-Button A: X+69, Y+23
-Button B: X+27, Y+71
-Prize: X=18641, Y=10279
-"""
-
-MACHINE_PATTERN = "A:.X([-+][0-9]*),.Y([-+][0-9]*)[^:]*:.X([-+][0-9]*),.Y([-+][0-9]*)[^:]*:.X=([0-9]*),.Y=([0-9]*)"
-MACHINE_RE = re.compile(MACHINE_PATTERN, re.DOTALL)
-
+from days.day_13.day_13 import Machine, parse_machines, TOKENS_A, TOKENS_B, EXAMPLE
 
 def solve_for_machine(machine: Machine) -> int:
     # a * a.x + b * b.x = p.x
